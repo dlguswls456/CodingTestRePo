3a12b271a3e710a78b763489d17f0d24b99b9767
HYUNJIN LEE <dlguswls456@naver.com>
2023-11-06 17:30:11 +0900

    이것이 코딩 테스트다 queue: DFS/BFS

---
+++ CodingTest/src/dfs_bfs/QueueExample.java
@@ -0,0 +1,24 @@
+package dfs_bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+
+	public static void main(String[] args) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+
+		queue.add(5);
+		queue.add(2);
+		queue.add(3);
+		queue.add(7);
+		queue.remove();
+		queue.add(1);
+		queue.add(4);
+		queue.remove();
+
+		System.out.print(queue);
+
+	}
+
+}
