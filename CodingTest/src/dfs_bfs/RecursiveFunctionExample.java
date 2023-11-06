c4318cb2271214694002397286ef0f63f6811d46
HYUNJIN LEE <dlguswls456@naver.com>
2023-11-06 17:30:27 +0900

    이것이 코딩 테스트다 재귀함수: DFS/BFS

---
+++ CodingTest/src/dfs_bfs/RecursiveFunctionExample.java
@@ -0,0 +1,18 @@
+package dfs_bfs;
+
+public class RecursiveFunctionExample {
+
+	public static void main(String[] args) {
+
+		System.out.print(factorial(5));
+	}
+
+	public static int factorial(int n) {
+		if (n == 1 || n == 0) {
+			return 1;
+		}
+
+		return n * factorial(n - 1);
+	}
+
+}
