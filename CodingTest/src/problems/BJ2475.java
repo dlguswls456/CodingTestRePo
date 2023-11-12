3e83bcfc1c015cd7b1a979df273051a3bc6c8fd4
HYUNJIN LEE <dlguswls456@naver.com>
2023-11-12 18:13:15 +0900

    백준 2475: 거듭제곱

---
+++ CodingTest/src/problems/BJ2475.java
@@ -0,0 +1,24 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2475 {
+	//°ÅµìÁ¦°ö
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int sum = 0;
+		while (st.hasMoreTokens()) {
+			int num = Integer.parseInt(st.nextToken());
+			sum += Math.pow(num, 2);
+		}
+
+		int result = sum % 10;
+		System.out.print(result);
+	}
+
+}
