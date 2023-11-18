65b05cea5c78e45fb0dc88f50bdf0b0e7e2b296e
HYUNJIN LEE <dlguswls456@naver.com>
2023-11-18 23:45:12 +0900

    SWEA 1284

---
+++ CodingTest/src/problems/SWEA1284.java
@@ -0,0 +1,36 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class SWEA1284 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int P = Integer.parseInt(st.nextToken());
+			int Q = Integer.parseInt(st.nextToken());
+			int R = Integer.parseInt(st.nextToken());
+			int S = Integer.parseInt(st.nextToken());
+			int W = Integer.parseInt(st.nextToken());
+
+			int priceA = W * P;
+			int priceB = Q;
+			if (W > R) {
+				priceB += (W - R) * S;
+			}
+
+			bw.write("#" + t + " " + Math.min(priceA, priceB));
+			bw.newLine();
+		}
+		bw.close();
+	}
+}
