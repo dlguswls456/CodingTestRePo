826f8aca76ed4c807452437a9def105dbac8c3d3
HYUNJIN LEE <dlguswls456@naver.com>
2024-02-06 01:28:07 +0900

    백준 11866: 큐

---
+++ CodingTest/src/datastructure/BJ11866.java
@@ -0,0 +1,40 @@
+package datastructure;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BJ11866 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 1; i <= N; i++) {
+			list.add(i);
+		}
+
+		String result = "<";
+		int idx = 0;
+		while (!list.isEmpty()) {
+			idx += K - 1;
+			while (idx >= list.size()) {
+				idx -= list.size();
+			}
+
+			result += list.remove(idx) + ", ";
+		}
+		result = result.substring(0, result.length()-2);
+		result += ">";
+		System.out.println(result);
+	}
+
+}
