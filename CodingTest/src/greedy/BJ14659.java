3d293930c0cb110d631db4fb3b810a3742e5e8e7
HYUNJIN LEE <dlguswls456@naver.com>
2024-02-13 23:05:35 +0900

    백준 14659: greedy

---
+++ CodingTest/src/greedy/BJ14659.java
@@ -0,0 +1,34 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ14659 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] heights = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			heights[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int result = 0;
+		for (int i = 0; i < N; i++) {
+			int tempResult = 0;
+			for (int j = i + 1; j < N; j++) {
+				if (heights[i] > heights[j]) {
+					tempResult++;
+				}else {
+					break;
+				}
+			}
+			result = Math.max(result, tempResult);
+		}
+		
+		System.out.println(result);
+	}
+}
