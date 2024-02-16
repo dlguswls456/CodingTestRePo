9632d0d607efd94e5b82d91c15896bdd556db384
HYUNJIN LEE <48899088+dlguswls456@users.noreply.github.com>
2024-02-16 10:48:33 +0900

    SWEA 1225: Queue

---
+++ CodingTest/src/datastructure/SWEA1225.java
@@ -0,0 +1,47 @@
+package ssafy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class SWEA1225 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int t = 1; t <= 10; t++) {
+			Integer.parseInt(br.readLine());
+			Queue<Integer> queue = new LinkedList<Integer>();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 8; i++) {
+				queue.add(Integer.parseInt(st.nextToken()));
+			}
+
+			int flag = 1;
+			while (true) {
+				int currentNum = queue.poll();
+				currentNum -= flag;
+				if (currentNum <= 0) {
+					queue.add(0);
+					break;
+				}
+				queue.add(currentNum);
+				flag++;
+				if (flag == 6) {
+					flag = 1;
+				}
+
+			}
+
+			System.out.print("#" + t + " ");
+			while (!queue.isEmpty()) {
+				System.out.print(queue.poll() + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
