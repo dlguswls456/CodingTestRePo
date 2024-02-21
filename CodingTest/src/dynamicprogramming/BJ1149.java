3648a3f1ca4aec8e5a2322663f41cb897c972e79
HYUNJIN LEE <48899088+dlguswls456@users.noreply.github.com>
2024-02-21 17:32:09 +0900

    백준 1149: dynamic programming

---
+++ CodingTest/src/dynamicprogramming/BJ1149.java
@@ -0,0 +1,37 @@
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1149 {
+
+	static int N;
+	static int[][] price;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		price = new int[N][3];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 3; j++) {
+				price[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int[][] dp = new int[N][3];
+		dp[0] = price[0];
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < 3; j++) {
+				dp[i][j] = price[i][j] + Math.min(dp[i - 1][(j + 1) % 3], dp[i - 1][(j + 2) % 3]);
+			}
+		}
+
+		int result = Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
+		System.out.println(result);
+
+	}
+
+}
