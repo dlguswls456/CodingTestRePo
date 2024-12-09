07505ad69b50cca7dee58ad53bd3a1c5cc2348c3
HYUNJIN LEE <48899088+dlguswls456@users.noreply.github.com>
2024-12-09 12:50:26 +0900

    [Silver V] Title: 집합, Time: 1564 ms, Memory: 313208 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N = 0;
+    static HashSet<Integer> set = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> fullSet = new HashSet<>();
+        for (int i = 1; i <= 20; i++) {
+            fullSet.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
+            switch (order) {
+                case "add":
+                    set.add(Integer.parseInt(st.nextToken()));
+                    break;
+                case "remove":
+                    set.remove(Integer.parseInt(st.nextToken()));
+                    break;
+                case "check":
+                    if (set.contains(Integer.parseInt(st.nextToken()))) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "toggle":
+                    int num = Integer.parseInt(st.nextToken());
+                    if (set.contains(num)) {
+                        set.remove(num);
+                    } else {
+                        set.add(num);
+                    }
+                    break;
+                case "all":
+                    set.addAll(fullSet);
+                    break;
+                case "empty":
+                    set = new HashSet<>();
+                    break;
+            }
+        }
+        System.out.print(sb);
+    }
+
+}
